528bd649fe6a2902e294b77082699f94ae170caa
Gordon Zhu <gordonmzhu@gmail.com>
2014-05-14 15:23:52 -0700

    Video 16

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,14 +7,23 @@ angular.module('myApp.controllers', [])
 
   }])
   .controller('WaitlistController', ['$scope', '$firebase', function($scope, $firebase) {
-    var partiesRef = new Firebase('https://waitandeat-demo.firebaseio.com/');
-
+    // Connect $scope.parties to live Firebase data
+    var partiesRef = new Firebase('https://waitandeat-demo.firebaseio.com/parties');
     $scope.parties = $firebase(partiesRef);
 
+    // Object to store data from the waitlist form.
     $scope.newParty = {name: '', phone: '', size: ''};
 
+    // Function to save a new party to the waitlist.
     $scope.saveParty = function() {
       $scope.parties.$add($scope.newParty);
       $scope.newParty = {name: '', phone: '', size: ''};
+    };
+
+    // Function to send a text message to a party.
+    $scope.sendTextMessage = function(phoneNumber) {
+      var textMessageRef = new Firebase('https://waitandeat-demo.firebaseio.com/textMessages');
+      var textMessages = $firebase(textMessageRef);
+      textMessages.$add({phoneNumber: phoneNumber});
     };
   }]);
