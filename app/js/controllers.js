e270ca1ac50b9f45e5e5e619b8ff4abafba4e484
Gordon Zhu <gordonmzhu@gmail.com>
2014-06-02 18:56:59 -0700

    Video 25

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -34,7 +34,7 @@ angular.module('myApp.controllers', [])
       $scope.parties.$save(party.$id);
     };
   }])
-  .controller('AuthController', ['$scope', '$firebaseSimpleLogin', function($scope, $firebaseSimpleLogin) {
+  .controller('AuthController', ['$scope', '$firebaseSimpleLogin', '$location', function($scope, $firebaseSimpleLogin, $location) {
     var authRef = new Firebase('https://waitandeat-demo.firebaseio.com/');
     var auth = $firebaseSimpleLogin(authRef);
 
@@ -43,17 +43,19 @@ angular.module('myApp.controllers', [])
     $scope.register = function() {
       auth.$createUser($scope.user.email, $scope.user.password).then(function(data) {
         console.log(data);
-        auth.$login('password', $scope.user);
+        $scope.login();
       });
     };
 
     $scope.login = function() {
       auth.$login('password', $scope.user).then(function(data) {
         console.log(data);
+        $location.path('/waitlist');
       });
     };
 
     $scope.logout = function() {
       auth.$logout();
+      $location.path('/');
     };
   }]);
